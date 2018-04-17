894489eff0215ea0658fa098a7724b048deb130a
alex323glo <alex323glo@gmail.com>
2018-04-17 17:32:20 +0300

    Add Applicatio 'start pont' class and simple REST Controller

---
+++ RESTful-web-service/src/main/java/org/alex323glo/its_simulator/Application.java
@@ -0,0 +1,17 @@
+package org.alex323glo.its_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by alex323glo on 17.04.18.
+ * TODO complete JavaDoc
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ RESTful-web-service/src/main/java/org/alex323glo/its_simulator/controller/StaticResourceController.java
@@ -0,0 +1,10 @@
+package org.alex323glo.its_simulator.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by alex323glo on 17.04.18.
+ */
+@RestController
+public class StaticResourceController {
+}
